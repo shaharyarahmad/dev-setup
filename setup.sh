cd0e9d8f75293e1a495730aa2c19d04b19f35a57
Shaharyar Ahmad <shaharyarahmad@users.noreply.github.com>
2018-05-27 18:48:13 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -18,7 +18,7 @@ apt-get update
 apt-get install -y php7.1
 apt-get install -y php-gd
 apt-get install -y php-curl
-apt-get install -y php-mcrypt
+apt-get install -y php-7.1mcrypt
 apt-get install -y php-mbstring
 apt-get install -y php-xml
 apt-get install -y php-zip
@@ -27,7 +27,7 @@ apt-get install -y php-zip
 apt-get install -y debconf-utils
 debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
 debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
-apt-get install mysql-server php7.1-mysql mysql-common
+sudo apt-get install mysql-server php7.1-mysql mysql-common
 mysql -u root -proot -e "CREATE USER 'sandbox'@'%' IDENTIFIED BY 'sandbox';"
 mysql -u root -proot -e "GRANT ALL PRIVILEGES ON *.* TO 'sandbox'@'%' WITH GRANT OPTION;"
 mysql -u root -proot -e "FLUSH PRIVILEGES;"
@@ -39,3 +39,6 @@ apt-get install -y git
 apt-get install -y vim
 apt-get install -y zip
 apt-get install -y unzip
+
+#Clean
+apt-get autoremove
